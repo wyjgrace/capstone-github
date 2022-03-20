6f0db83dfd13a6b79fbd6ef048c75233e42e1d0c
wyjgrace <wyingjiagrace@gmail.com>
2022-03-20 14:21:00 +0800

    Update test_script.js

--- test_script.js
+++ test_script.js
@@ -1,13 +1,7 @@
-{/* <script>
-  alert('hello world!')
-</script> */}
 
-alert('hello world!')
+// alert('hello world!')
+var body=document.getElementsByTagName('head')[0];
+console.log(body); 
 
-// javascript: (function() {    })();
-
-// javascript: (() => {  alert('Hello, World!');})();
-
-// javascript: (() => { var js = document.createElement('script');    js.setAttribute('src', 'http://127.0.0.1:8000/test_script.js');    document.body.appendChild(js); })();
-
+// put this line of code at bookmark's URL/location. 
 //javascript:(function(){document.body.appendChild(document.createElement('script')).src='http://127.0.0.1:8000/test_script.js';})();
