274d50e0542d9db76f772d27c5a344ab35fadc0c
wyjgrace <wyingjiagrace@gmail.com>
2022-03-20 14:15:57 +0800

    Update README and test_script.js

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # capstone-github
  
+**Run javascript on http server**
+1. open cmd terminal
+2. cd to directory containing the javascript file (C:\Users\wying\Desktop\capstone-github\)
+3. type 'python -m http.server' to start local http server
+4. js file is hosted here: http://127.0.0.1:8000/test_script.js
+
+why do i need to do this?
+- chrome bookmarklet 'not allowed to load local resource' error
+- this is a security exception built into Chrome and other modern browsers.
--- test_script.js
+++ test_script.js
@@ -1,11 +1,13 @@
-<script type="text/javascript" src="test_script.js">
-  alert('hello world!');
-</script>
+{/* <script>
+  alert('hello world!')
+</script> */}
+
+alert('hello world!')
 
 // javascript: (function() {    })();
 
 // javascript: (() => {  alert('Hello, World!');})();
 
-// javascript: (() => { var js = document.createElement('script');    js.setAttribute('src', 'C:\Users\wying\Desktop\fyp_code\test_script.js');    document.body.appendChild(js); })();
+// javascript: (() => { var js = document.createElement('script');    js.setAttribute('src', 'http://127.0.0.1:8000/test_script.js');    document.body.appendChild(js); })();
 
-//javascript:(function(){document.body.appendChild(document.createElement('script')).src='C:\Users\wying\Desktop\fyp_code\test_script.js';})();
+//javascript:(function(){document.body.appendChild(document.createElement('script')).src='http://127.0.0.1:8000/test_script.js';})();
