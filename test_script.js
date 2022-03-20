44e633f6a821f501ebbb64a455eba4459140058f
wyjgrace <wyingjiagrace@gmail.com>
2022-03-20 14:05:44 +0800

    Create test_script.js

---
+++ test_script.js
@@ -0,0 +1,11 @@
+<script type="text/javascript" src="test_script.js">
+  alert('hello world!');
+</script>
+
+// javascript: (function() {    })();
+
+// javascript: (() => {  alert('Hello, World!');})();
+
+// javascript: (() => { var js = document.createElement('script');    js.setAttribute('src', 'C:\Users\wying\Desktop\fyp_code\test_script.js');    document.body.appendChild(js); })();
+
+//javascript:(function(){document.body.appendChild(document.createElement('script')).src='C:\Users\wying\Desktop\fyp_code\test_script.js';})();
