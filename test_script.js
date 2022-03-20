91842436dc006052c4bd6dd72105a09c1dd673db
wyjgrace <wyingjiagrace@gmail.com>
2022-03-20 15:59:45 +0800

    Update test_script.js

--- test_script.js
+++ test_script.js
@@ -1,7 +1,11 @@
 
 // alert('hello world!')
-var body=document.getElementsByTagName('nav')[0];
-console.log(body); 
+document.addEventListener("DOMContentLoaded", () => {
+  // alert("DOM ready!");
+  var body=document.getElementsByTagName('nav')[0];
+  console.log(body); 
+});
+
 
 // put this line of code at bookmark's URL/location. 
 //javascript:(function(){document.body.appendChild(document.createElement('script')).src='http://127.0.0.1:8000/test_script.js';})();
